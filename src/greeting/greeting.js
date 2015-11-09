d63fd471b29fff156d602d5e7f4535f635923903
belgac <hans.scheuren@bizliner.com>
2015-11-09 16:19:47 +0000

    docs(greeting): add comments to greeting module
    add the comments to the greeting module

--- src/greeting/greeting.js
+++ src/greeting/greeting.js
@@ -1,4 +1,6 @@
+//call the module to greet the user with package info
 var greetPackageInfo = require('./greetPackageInfo/greetPackageInfo.js');
+//call the module to generate a list of available tasks
 var taskList = require('../taskList/taskList.js')
 
 //export the module
@@ -6,10 +8,12 @@ module.exports = greeting;
 
 //create the greeting function
 function greeting(packageInfo,tasks){
-  
+  //init the greeting message string
   var greetingMessage = ''
+  //get the greeting including package Info
   greetingMessage += greetPackageInfo(packageInfo);
+  //get the list of available tasks
   greetingMessage += taskList(tasks);
-  
+  //return the greeting message
   return greetingMessage;
 }
